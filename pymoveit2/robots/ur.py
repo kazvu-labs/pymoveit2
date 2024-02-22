156f62b1583e873c23be27e3c12ffafa1a94407a
Dan Isla <dan.isla@gmail.com>
2024-02-22 14:25:00 -0800

    add prefix and robot name to ur

--- pymoveit2/robots/ur.py
+++ pymoveit2/robots/ur.py
@@ -5,24 +5,26 @@ MOVE_GROUP_ARM: str = "ur_manipulator"
 OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [0.04, 0.04]
 CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0]
 
+def move_group_arm(prefix="", robot_name="ur"):
+    return f"{prefix}{robot_name}_manipulator"
 
-def joint_names() -> List[str]:
+def joint_names(prefix="") -> List[str]:
     return [
-        "shoulder_pan_joint",
-        "shoulder_lift_joint",
-        "elbow_joint",
-        "wrist_1_joint",
-        "wrist_2_joint",
-        "wrist_3_joint"
+        f"{prefix}shoulder_pan_joint",
+        f"{prefix}shoulder_lift_joint",
+        f"{prefix}elbow_joint",
+        f"{prefix}wrist_1_joint",
+        f"{prefix}wrist_2_joint",
+        f"{prefix}wrist_3_joint"
     ]
 
 
-def base_link_name() -> str:
-    return "base_link"
+def base_link_name(prefix="") -> str:
+    return f"{prefix}base_link"
 
 
-def end_effector_name() -> str:
-    return "tool0"
+def end_effector_name(prefix="") -> str:
+    return f"{prefix}tool0"
 
 
 def gripper_joint_names() -> List[str]:
