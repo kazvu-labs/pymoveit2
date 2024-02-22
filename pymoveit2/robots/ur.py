2c152d7a12a275bb023a8f933059047a498bf5af
Dan Isla <dan.isla@gmail.com>
2024-02-22 12:57:28 -0800

    import ur robot type

---
+++ pymoveit2/robots/ur.py
@@ -0,0 +1,29 @@
+from typing import List
+
+MOVE_GROUP_ARM: str = "ur_manipulator"
+
+OPEN_GRIPPER_JOINT_POSITIONS: List[float] = [0.04, 0.04]
+CLOSED_GRIPPER_JOINT_POSITIONS: List[float] = [0.0, 0.0]
+
+
+def joint_names() -> List[str]:
+    return [
+        "shoulder_pan_joint",
+        "shoulder_lift_joint",
+        "elbow_joint",
+        "wrist_1_joint",
+        "wrist_2_joint",
+        "wrist_3_joint"
+    ]
+
+
+def base_link_name() -> str:
+    return "base_link"
+
+
+def end_effector_name() -> str:
+    return "tool0"
+
+
+def gripper_joint_names() -> List[str]:
+    return []
